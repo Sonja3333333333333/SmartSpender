988e9c6ef583993dc66b0cd95c5deffe7bbd874e
nataliapopko <nata.popko06@gmail.com>
2026-04-14 01:40:17 +0300

    додано вікна додавання операцій

--- app/src/main/java/com/example/smartspend/DashboardFragment.java
+++ app/src/main/java/com/example/smartspend/DashboardFragment.java
@@ -1,64 +1,73 @@
 package com.example.smartspend;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DashboardFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import java.util.ArrayList;
+
 public class DashboardFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public DashboardFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DashboardFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DashboardFragment newInstance(String param1, String param2) {
-        DashboardFragment fragment = new DashboardFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dashboard, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        View pieChart = view.findViewById(R.id.pieChart);
+        View balanceGroup = view.findViewById(R.id.balance_group);
+        View emptyDataParent = view.findViewById(R.id.empty_data_parent);
+
+        // умова для відображення плейсхолдера якщо нема даних для pie chart
+        boolean hasData = false;
+
+        if (hasData) {
+            pieChart.setVisibility(View.VISIBLE);
+            balanceGroup.setVisibility(View.VISIBLE);
+            emptyDataParent.setVisibility(View.GONE);
+        } else {
+            // Ховаємо діаграму та баланс
+            pieChart.setVisibility(View.GONE);
+            balanceGroup.setVisibility(View.GONE);
+
+            // Показуємо відразу весь блок з плейсхолдером, текстом і стрілкою
+            emptyDataParent.setVisibility(View.VISIBLE);
+        }
+
+        //кнопка додати операцію
+        ImageView btnAdd = view.findViewById(R.id.btn_add_transaction);
+
+        if (btnAdd != null) {
+            btnAdd.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getActivity(), AddTransactionActivity.class);
+                    startActivity(intent);
+
+                    if (getActivity() != null) {
+                        getActivity().overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                    }
+                }
+            });
+        }
     }
 }
